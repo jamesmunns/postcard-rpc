bc2a12bdcdee4a8f83099e0869c9d6268e93995f
James Munns <james@onevariable.com>
2023-11-14 00:58:40 +0100

    remove dead code

--- james-stack/host-client/src/bin/demo.rs
+++ james-stack/host-client/src/bin/demo.rs
@@ -1,36 +1,7 @@
-use std::{
-    sync::{atomic::AtomicBool, Arc},
-    time::Duration,
-};
+use std::time::Duration;
 
-use host_client::{io_thread, serial, HostClient};
-use james_icd::{FatalError, Sleep, SleepDone};
-use pd_core::{headered::to_slice_cobs, Dispatch, WireHeader};
-use tokio::time::sleep;
-
-struct Context {}
-
-#[derive(Debug)]
-enum CommsError {}
-
-const SLEEP_PATH: &str = "sleep";
-const ERROR_PATH: &str = "error";
-
-fn sleep_resp_handler(hdr: &WireHeader, _c: &mut Context, buf: &[u8]) -> Result<(), CommsError> {
-    match postcard::from_bytes::<SleepDone>(buf) {
-        Ok(m) => println!(" -> Got({}:{:?}): {m:?}", hdr.seq_no, hdr.key),
-        Err(_) => println!("sleep done fail"),
-    }
-    Ok(())
-}
-
-fn error_resp_handler(hdr: &WireHeader, _c: &mut Context, buf: &[u8]) -> Result<(), CommsError> {
-    match postcard::from_bytes::<FatalError>(buf) {
-        Ok(m) => println!(" -> Got({}:{:?}): {m:?}", hdr.seq_no, hdr.key),
-        Err(_) => println!("sleep done fail"),
-    }
-    Ok(())
-}
+use host_client::HostClient;
+use james_icd::{Sleep, SleepDone};
 
 #[tokio::main]
 async fn main() {
