f79120b950415c999c8af7c429f15d75ae9bcd5d
James Munns <james@onevariable.com>
2024-05-03 08:56:14 +0200

    target_server: Add support for "context" fields, as well as subscriptions (#12)
    * Add context to dispatcher
    * Hmm, spawn can't take context...
    * Handle spawn context, split out sender
    * Oops default features
    * Add publish method to sender

--- source/postcard-rpc/src/target_server/mod.rs
+++ source/postcard-rpc/src/target_server/mod.rs
@@ -3,22 +3,50 @@
 use crate::{
     headered::extract_header_from_bytes,
     standard_icd::{FrameTooLong, FrameTooShort, WireError},
-    Key, WireHeader,
+    WireHeader,
 };
-use embassy_sync::{blocking_mutex::raw::RawMutex, mutex::Mutex};
+use embassy_sync::blocking_mutex::raw::RawMutex;
 use embassy_usb::{
-    driver::{Driver, Endpoint, EndpointError, EndpointIn, EndpointOut},
+    driver::Driver,
     msos::{self, windows_version},
     Builder, UsbDevice,
 };
-use postcard::experimental::schema::Schema;
-use serde::Serialize;
-use static_cell::StaticCell;
+use embassy_usb_driver::{Endpoint, EndpointError, EndpointOut};
+use sender::Sender;
 
 pub mod buffers;
 pub mod dispatch_macro;
+pub mod sender;
 
 const DEVICE_INTERFACE_GUIDS: &[&str] = &["{AFB9A6FB-30BA-44BC-9232-806CFC875321}"];
+
+/// A trait that defines the postcard-rpc message dispatching behavior
+///
+/// This is normally generated automatically by the [`define_dispatch!()`]
+/// macro.
+pub trait Dispatch {
+    type Mutex: RawMutex;
+    type Driver: Driver<'static>;
+
+    /// Handle a single message, with the header deserialized and the
+    /// body not yet deserialized.
+    ///
+    /// This function must handle replying (either immediately or
+    /// in the future, for example if spawning a task)
+    async fn dispatch(&mut self, hdr: WireHeader, body: &[u8]);
+
+    /// Send an error message, of the path and key defined for
+    /// the connection
+    async fn error(&self, seq_no: u32, error: WireError);
+
+    /// Obtain an owned sender
+    fn sender(&self) -> Sender<Self::Mutex, Self::Driver>;
+}
+
+pub trait SpawnContext {
+    type SpawnCtxt: 'static;
+    fn spawn_ctxt(&mut self) -> Self::SpawnCtxt;
+}
 
 pub fn example_config() -> embassy_usb::Config<'static> {
     // Create embassy-usb Config
@@ -79,123 +107,9 @@ pub fn configure_usb<D: embassy_usb::driver::Driver<'static>>(
     (usb, ep_in, ep_out)
 }
 
-// comms
-
-pub trait Dispatch {
-    type Mutex: RawMutex;
-    type Driver: Driver<'static>;
-
-    async fn dispatch(
-        &self,
-        hdr: WireHeader,
-        body: &[u8],
-        sender: Sender<Self::Mutex, Self::Driver>,
-    );
-    async fn error(&self, seq_no: u32, error: WireError, sender: Sender<Self::Mutex, Self::Driver>);
-    fn sender(&self) -> Sender<Self::Mutex, Self::Driver>;
-}
-
-#[derive(Copy)]
-pub struct Sender<M: RawMutex + 'static, D: Driver<'static> + 'static> {
-    inner: &'static Mutex<M, SenderInner<D>>,
-}
-
-impl<M: RawMutex + 'static, D: Driver<'static> + 'static> Sender<M, D> {
-    #[inline]
-    pub async fn reply<E>(&self, seq_no: u32, resp: &E::Response) -> Result<(), ()>
-    where
-        E: crate::Endpoint,
-        E::Response: Serialize + Schema,
-    {
-        let mut inner = self.inner.lock().await;
-        let SenderInner { ep_in, tx_buf } = &mut *inner;
-        reply_keyed::<D, E::Response>(ep_in, E::RESP_KEY, seq_no, resp, tx_buf).await
-    }
-
-    #[inline]
-    pub async fn reply_keyed<T>(&self, seq_no: u32, key: Key, resp: &T) -> Result<(), ()>
-    where
-        T: Serialize + Schema,
-    {
-        let mut inner = self.inner.lock().await;
-        let SenderInner { ep_in, tx_buf } = &mut *inner;
-        reply_keyed::<D, T>(ep_in, key, seq_no, resp, tx_buf).await
-    }
-}
-
-impl<M: RawMutex + 'static, D: Driver<'static> + 'static> Clone for Sender<M, D> {
-    fn clone(&self) -> Self {
-        Sender { inner: self.inner }
-    }
-}
-
-pub struct SenderInner<D: Driver<'static>> {
-    ep_in: D::EndpointIn,
-    tx_buf: &'static mut [u8],
-}
-
-#[inline]
-async fn reply_keyed<D, T>(
-    ep_in: &mut D::EndpointIn,
-    key: Key,
-    seq_no: u32,
-    resp: &T,
-    out: &mut [u8],
-) -> Result<(), ()>
-where
-    D: Driver<'static>,
-    T: Serialize + Schema,
-{
-    if let Ok(used) = crate::headered::to_slice_keyed(seq_no, key, resp, out) {
-        send_all::<D>(ep_in, used).await
-    } else {
-        Err(())
-    }
-}
-
-#[inline]
-async fn send_all<D>(ep_in: &mut D::EndpointIn, out: &[u8]) -> Result<(), ()>
-where
-    D: Driver<'static>,
-{
-    if out.is_empty() {
-        return Ok(());
-    }
-    ep_in.wait_enabled().await;
-    // write in segments of 64. The last chunk may
-    // be 0 < len <= 64.
-    for ch in out.chunks(64) {
-        if ep_in.write(ch).await.is_err() {
-            return Err(());
-        }
-    }
-    // If the total we sent was a multiple of 64, send an
-    // empty message to "flush" the transaction
-    if (out.len() & (64 - 1)) == 0 && ep_in.write(&[]).await.is_err() {
-        return Err(());
-    }
-
-    Ok(())
-}
-
-impl<M, D> Sender<M, D>
-where
-    M: RawMutex + 'static,
-    D: Driver<'static> + 'static,
-{
-    pub fn init_sender(
-        sc: &'static StaticCell<Mutex<M, SenderInner<D>>>,
-        tx_buf: &'static mut [u8],
-        ep_in: D::EndpointIn,
-    ) -> Self {
-        let x = sc.init(Mutex::new(SenderInner { ep_in, tx_buf }));
-        Sender { inner: x }
-    }
-}
-
 pub async fn rpc_dispatch<M, D, T>(
     mut ep_out: D::EndpointOut,
-    dispatch: T,
+    mut dispatch: T,
     rx_buf: &'static mut [u8],
 ) -> !
 where
@@ -203,7 +117,6 @@ where
     D: Driver<'static> + 'static,
     T: Dispatch<Mutex = M, Driver = D>,
 {
-    let sender = dispatch.sender();
     'connect: loop {
         // Wait for connection
         ep_out.wait_enabled().await;
@@ -227,7 +140,7 @@ where
                                         .unwrap_or(u32::MAX),
                                     max: u32::try_from(rx_buf.len()).unwrap_or(u32::MAX),
                                 });
-                                dispatch.error(0, err, sender.clone()).await;
+                                dispatch.error(0, err).await;
                                 continue 'packet;
                             }
                             Ok(n) => {
@@ -262,12 +175,12 @@ where
 
             // If it's for us, process it
             if let Ok((hdr, body)) = extract_header_from_bytes(frame) {
-                dispatch.dispatch(hdr, body, sender.clone()).await;
+                dispatch.dispatch(hdr, body).await;
             } else {
                 let err = WireError::FrameTooShort(FrameTooShort {
                     len: u32::try_from(frame.len()).unwrap_or(u32::MAX),
                 });
-                dispatch.error(0, err, sender.clone()).await;
+                dispatch.error(0, err).await;
             }
         }
     }
