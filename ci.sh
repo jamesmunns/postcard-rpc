e0306b58733e1998f90b6a88501f480578396973
James Munns <james@onevariable.com>
2025-07-16 17:48:54 +0200

    Enable embassy-time for test-utils feature

--- ci.sh
+++ ci.sh
@@ -60,13 +60,13 @@ cargo check \
 # Run stub test checks
 cargo test \
     --manifest-path source/postcard-rpc/Cargo.toml \
-    --features=embassy-usb-0_3-server
+    --features=embassy-usb-0_3-server,test-utils
 cargo test \
     --manifest-path source/postcard-rpc/Cargo.toml \
-    --features=embassy-usb-0_4-server
+    --features=embassy-usb-0_4-server,test-utils
 cargo test \
     --manifest-path source/postcard-rpc/Cargo.toml \
-    --features=embassy-usb-0_5-server
+    --features=embassy-usb-0_5-server,test-utils
 
 # Example projects
 cargo build \
